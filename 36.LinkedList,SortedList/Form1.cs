abfd2f70c39c13fdc623563f093a934fe4825f91
gangje <jinandkid173817@gmail.com>
2025-01-26 18:16:19 +0900

    01월 26일

---
+++ 36.LinkedList,SortedList/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _36.LinkedList_SortedList
+{
+    public partial class Form1 : Form
+    {
+        SortedList<DateTime, string> slScheduler = new SortedList<DateTime, string>();
+        public  Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnScheduler_Click(object sender, EventArgs e)
+        {
+            // Linked List
+
+            //LinkedList<string> sLinkTest = new LinkedList<string>();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    sLinkTest.AddLast(i.ToString());
+            //}
+
+            //var findnode = sLinkTest.Find("5");
+            //sLinkTest.AddAfter(findnode, "NewValue");
+
+            // Sorted List
+
+
+
+        }
+    }
+}
