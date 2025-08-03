ef5549ec524a35f486f86b49d9e2abab1d7669ee
hyunjae <Shj229@naver.com>
2025-08-03 21:40:20 +0900

    8/03

--- API/Form1.Designer.cs
+++ API/Form1.Designer.cs
@@ -35,11 +35,10 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(223, 95);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(800, 450);
+            this.dataGridView1.Size = new System.Drawing.Size(400, 285);
             this.dataGridView1.TabIndex = 0;
             // 
             // Form1
---
+++ API/Form1.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDataAsync(); // 폼 로드시 API 데이터 가져오기
+        }
+
+        private async void LoadDataAsync()
+        {
+            try
+            {
+                string apiUrl = "https://jsonplaceholder.typicode.com/users";
+                string json = await client.GetStringAsync(apiUrl);
+
+                List<User> users = JsonSerializer.Deserialize<List<User>>(json);
+
+                // DataGridView에 바인딩
+                dataGridView1.DataSource = users;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("❌ API 호출 실패: " + ex.Message);
+            }
+        }
+    }
+
+    // API에서 반환되는 JSON 구조에 맞춰 클래스 생성
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
