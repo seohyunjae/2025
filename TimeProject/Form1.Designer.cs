d933e32a9dbd91790efe8dc4ac6b59d11963b786
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2025-12-11 22:42:43 +0900

    12/11 Time Program

---
+++ TimeProject/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace TimeProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Date = new System.Windows.Forms.DateTimePicker();
+			this.Datefrtime = new System.Windows.Forms.DateTimePicker();
+			this.Dateendtime = new System.Windows.Forms.DateTimePicker();
+			this.cboactivity = new System.Windows.Forms.ComboBox();
+			this.buttonrigist = new System.Windows.Forms.Button();
+			this.Date1 = new System.Windows.Forms.DateTimePicker();
+			this.buttonretrieve = new System.Windows.Forms.Button();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.remark = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 27;
+			this.dataGridView1.Size = new System.Drawing.Size(1145, 332);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// Date
+			// 
+			this.Date.Location = new System.Drawing.Point(13, 381);
+			this.Date.Name = "Date";
+			this.Date.Size = new System.Drawing.Size(200, 25);
+			this.Date.TabIndex = 1;
+			// 
+			// Datefrtime
+			// 
+			this.Datefrtime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.Datefrtime.Location = new System.Drawing.Point(219, 381);
+			this.Datefrtime.Name = "Datefrtime";
+			this.Datefrtime.Size = new System.Drawing.Size(200, 25);
+			this.Datefrtime.TabIndex = 2;
+			// 
+			// Dateendtime
+			// 
+			this.Dateendtime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.Dateendtime.Location = new System.Drawing.Point(425, 381);
+			this.Dateendtime.Name = "Dateendtime";
+			this.Dateendtime.Size = new System.Drawing.Size(200, 25);
+			this.Dateendtime.TabIndex = 2;
+			// 
+			// cboactivity
+			// 
+			this.cboactivity.FormattingEnabled = true;
+			this.cboactivity.Location = new System.Drawing.Point(631, 381);
+			this.cboactivity.Name = "cboactivity";
+			this.cboactivity.Size = new System.Drawing.Size(121, 23);
+			this.cboactivity.TabIndex = 3;
+			// 
+			// buttonrigist
+			// 
+			this.buttonrigist.Location = new System.Drawing.Point(758, 381);
+			this.buttonrigist.Name = "buttonrigist";
+			this.buttonrigist.Size = new System.Drawing.Size(109, 80);
+			this.buttonrigist.TabIndex = 4;
+			this.buttonrigist.Text = "스케줄 등록";
+			this.buttonrigist.UseVisualStyleBackColor = true;
+			this.buttonrigist.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Date1
+			// 
+			this.Date1.Location = new System.Drawing.Point(13, 467);
+			this.Date1.Name = "Date1";
+			this.Date1.Size = new System.Drawing.Size(200, 25);
+			this.Date1.TabIndex = 5;
+			// 
+			// buttonretrieve
+			// 
+			this.buttonretrieve.Location = new System.Drawing.Point(219, 467);
+			this.buttonretrieve.Name = "buttonretrieve";
+			this.buttonretrieve.Size = new System.Drawing.Size(109, 31);
+			this.buttonretrieve.TabIndex = 6;
+			this.buttonretrieve.Text = "조회";
+			this.buttonretrieve.UseVisualStyleBackColor = true;
+			this.buttonretrieve.Click += new System.EventHandler(this.buttonretrieve_Click);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			// 
+			// remark
+			// 
+			this.remark.Location = new System.Drawing.Point(13, 413);
+			this.remark.Multiline = true;
+			this.remark.Name = "remark";
+			this.remark.Size = new System.Drawing.Size(739, 48);
+			this.remark.TabIndex = 8;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1170, 504);
+			this.Controls.Add(this.remark);
+			this.Controls.Add(this.buttonretrieve);
+			this.Controls.Add(this.Date1);
+			this.Controls.Add(this.buttonrigist);
+			this.Controls.Add(this.cboactivity);
+			this.Controls.Add(this.Dateendtime);
+			this.Controls.Add(this.Datefrtime);
+			this.Controls.Add(this.Date);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker Date;
+        private System.Windows.Forms.DateTimePicker Datefrtime;
+        private System.Windows.Forms.DateTimePicker Dateendtime;
+        private System.Windows.Forms.ComboBox cboactivity;
+        private System.Windows.Forms.Button buttonrigist;
+        private System.Windows.Forms.DateTimePicker Date1;
+        private System.Windows.Forms.Button buttonretrieve;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox remark;
+    }
+}
+
---
+++ TimeProject/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace TimeProject
+{
+    public partial class Form1 : Form
+    {
+		private readonly string connectionString =@"Server=localhost;Database=TimeDB;Integrated Security=True;";
+		public Form1()
+        {
+            InitializeComponent();
+			this.Load += Form1_Load;
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			// 날짜 기본값 오늘
+			Date.Value = DateTime.Today;
+			Datefrtime.Value = DateTime.Today;
+			Dateendtime.Value = DateTime.Today;
+
+			// Activity 콤보박스 채우기
+			LoadActivityCombo();
+		}
+		private void LoadActivityCombo()
+		{
+			string sql = "SELECT Id, CategoryName FROM Activity ORDER BY CategoryName;";
+
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			using (SqlCommand command = new SqlCommand(sql, connection))
+			{
+				DataTable table = new DataTable();
+				connection.Open();
+
+				using (SqlDataReader reader = command.ExecuteReader())
+				{
+					table.Load(reader);
+				}
+
+				cboactivity.DisplayMember = "CategoryName"; // 화면에 보이는 값
+				cboactivity.ValueMember = "Id";             // 실제 선택되는 값
+				cboactivity.DataSource = table;
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			// 1) 기본 유효성 체크 -----------------------------
+			if (cboactivity.SelectedValue == null)
+			{
+				MessageBox.Show("활동(카테고리)을 선택해 주세요.");
+				return;
+			}
+
+			// 날짜
+			DateTime date = Date.Value.Date;
+
+			// 날짜 + 시간 합치기 (시작/종료)
+			DateTime startTime = date.Date + Datefrtime.Value.TimeOfDay;
+			DateTime endTime = date.Date + Dateendtime.Value.TimeOfDay;
+
+			if (endTime <= startTime)
+			{
+				MessageBox.Show("끝나는 시간이 시작 시간보다 같거나 빠를 수 없습니다.");
+				return;
+			}
+
+			int activityCategoryId = (int)cboactivity.SelectedValue;
+			string txtremark = remark.Text.Trim();
+
+			// 2) INSERT 쿼리 준비 -----------------------------
+
+			string sql = @"
+                INSERT INTO TimeLog
+                    (Date, StartTime, EndTime, ActivityCategoryId, REMARK)
+                VALUES
+                    (@Date, @StartTime, @EndTime, @ActivityCategoryId, @Remark);
+            ";
+
+			// 3) DB 연결 후 실행 -----------------------------
+
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			using (SqlCommand command = new SqlCommand(sql, connection))
+			{
+				command.Parameters.AddWithValue("@Date", date);
+				command.Parameters.AddWithValue("@StartTime", startTime);
+				command.Parameters.AddWithValue("@EndTime", endTime);
+				command.Parameters.AddWithValue("@ActivityCategoryId", activityCategoryId);
+
+				if (string.IsNullOrEmpty(remark.ToString()))
+					command.Parameters.AddWithValue("@Remark", DBNull.Value);
+				else
+					command.Parameters.AddWithValue("@Remark", remark.Text.ToString());
+
+				connection.Open();
+				int rows = command.ExecuteNonQuery();
+
+				MessageBox.Show($"{rows}개의 스케줄이 저장되었습니다.");
+			}
+
+			// 4) 입력칸 정리 (메모만 지우고 싶다면 여기서 처리) -----
+
+			remark.Clear();
+
+			// TODO: 위에 큰 DataGridView가 있으면 여기서 다시 조회 함수 호출하면 됨.
+			// LoadTimeLogGrid(); 같은 함수 만들어서 호출해도 좋고.
+		}
+
+        private void buttonretrieve_Click(object sender, EventArgs e)
+        {
+			DateTime selectedDate = Date1.Value.Date;
+
+			// 2) 해당 날짜의 TimeLog + Activity JOIN해서 가져오는 쿼리
+			string sql = @"
+						SELECT  
+							--T.Id,
+							T.Date,
+							T.StartTime,
+							T.EndTime,
+							A.CategoryName,
+							A.ActivityType,
+							T.REMARK
+						FROM TimeLog T
+						INNER JOIN Activity A
+							ON T.ActivityCategoryId = A.Id
+						WHERE T.Date = @Date
+						ORDER BY T.StartTime;
+					";
+
+			// 3) DB 연결해서 DataTable에 담기
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			using (SqlCommand command = new SqlCommand(sql, connection))
+			{
+				command.Parameters.AddWithValue("@Date", selectedDate);
+
+				DataTable table = new DataTable();
+
+				connection.Open();
+				using (SqlDataReader reader = command.ExecuteReader())
+				{
+					table.Load(reader);
+				}
+
+				// 4) 그리드에 바인딩
+				dataGridView1.DataSource = table;
+				dataGridView1.AutoResizeColumns();
+			}
+		}
+    }
+}
